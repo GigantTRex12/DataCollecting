84a5026a5c9d5f79f6408892def07216a167eeee
GigantTRex12 <GigantTRex12@gmx.de>
2025-10-25 16:47:08 +0200

    some more javadoc

--- src/main/java/exceptions/InvalidInputFormatException.java
+++ src/main/java/exceptions/InvalidInputFormatException.java
@@ -2,6 +2,9 @@ package exceptions;
 
 import java.io.IOException;
 
+/**
+ * Exception allowed to be thrown by normalizers in {@link collector.Question}.
+ */
 public class InvalidInputFormatException extends IOException {
     public InvalidInputFormatException(String errorMessage) {
         super(errorMessage);
